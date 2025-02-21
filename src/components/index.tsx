dd4ec04cda5e30b069a22bdd2ff036987b64ab93
Ela <teodocio.am@gmail.com>
2025-02-21 16:30:10 -0600

    add exports for component directory

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,11 @@
 // houses all the exports within the component directory. for organization.
 
-export { default as Button } from './RetroButton';
-
+export * from './RetroButton';
+export * from './RetroCard';
+export * from './RetroDropdown';
+export * from './RetroHeroImage';
+export * from './RetroImage';
+export * from './RetroLabel';
+export * from './RetroRadio';
+export * from './RetroTable';
+export * from './RetroText';
